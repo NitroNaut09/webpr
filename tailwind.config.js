85d6811acd3ea7657ff39aba974b67cbadf51929
NitroNaut <132362957+NitroNaut09@users.noreply.github.com>
2025-01-14 15:52:25 +0530

    Add Tailwind CSS setup and various configuration files with NPM

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html", // All HTML files in the templates directory
+    "./core/templates/**/*.html", // If you have app-specific templates
+    "./static/**/*.css", // If you have standalone CSS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
